b9140295421ad70a91da4e19196fff3409bab30e
GuiFonsec <124678448+GuiFonsec@users.noreply.github.com>
2023-09-27 10:06:54 -0300

    Update forca.js

--- forca.js
+++ forca.js
@@ -1,15 +1,5 @@
-const numeroJogadores = window.prompt('Quantas pessoas vão jogar? (Máximo de jogadores:4)')
-/*switch (numeroJogadores) {
-    case 1:
-        
-        break;
-
-    default:
-        break;
-}*/
-
-const animais = ['Animais','cachorro', 'gato', 'elefante', 'leão', 'tigre', 'girafa', 'zebra', 'gorila', 'panda', 'cavalo', 
-'coelho', 'papagaio', 'penguim', 'jacaré', 'tubarão', 'pombo', 'urso polar', 'hipopótamo', 'camelo', 'vaca', 'baleia', 'cobra', 'macaco', 'avestruz', 'foca', 'raposa', 'guepardo', 'rinoceronte', 'lobo']
+//Temas do jogo.
+const animais = ['Animais','cachorro', 'gato', 'elefante', 'leão', 'tigre', 'girafa', 'zebra', 'gorila', 'panda', 'cavalo', 'coelho', 'papagaio', 'penguim', 'jacaré', 'tubarão', 'pombo', 'urso polar', 'hipopótamo', 'camelo', 'vaca', 'baleia', 'cobra', 'macaco', 'avestruz', 'foca', 'raposa', 'guepardo', 'rinoceronte', 'lobo']
 const comidas = ['Comidas','pizza', 'hambúrguer', 'sushi', 'massa', 'cuscuz', 'sorvete', 'lasanha', 'taco', 'sanduíche', 'batata frita', 'espaguete', 'salmão', 'frango grelhado', 'peixe e fritas', 'sopa de tomate', 'churrasco', 'torta de maçã', 'pudim', 'queijo', 'arroz frito', 'macarrão instantâneo', 'panqueca', 'salmão defumado', 'risoto', 'feijoada', 'croissant', 'guacamole']
 const capitaisBrasileiras = ['Capitais Brasileiras','aracaju', 'sao paulo', 'rio de janeiro', 'salvador', 'rio branco', 'belem', 'belo horizonte', 'boa vista', 'brasília', 'campo grande', 'cuiabá', 'curitiba', 'florianópolis', 'fortaleza', 'goiânia', 'joão pessoa', 'macapá', 'maceió', 'manaus', 'natal', 'palmas', 'porto alegre', 'porto velho', 'recife', 'são luís', 'teresina', 'vitoria' ]
 const linguagensDeProgramacao= ['Linguagens de programação','python', 'javascript', 'java', 'c', 'c++', 'c#', 'ruby', 'php', 'swift', 'go', 'rust', 'typescript', 'kotlin', 'perl', 'haskell', 'cobol', 'prolog', 'sql', 'assembly']
@@ -21,18 +11,24 @@ const instrumentosMusicais = ['Instrumentos Musicais','violino', 'guitarra', 'pi
 const frutas = ['Frutas', 'Banana', 'Pera', 'Uva', 'Tangerina', 'Tamarindo', 'Siriguela', 'Pitanga', 'Pessego', 'Morango', 'Melao', 'Melancia', 'Maracuja', 'Mangaba', 'Manga', 'Mamao', 'Maca', 'Limao', 'Laranja', 'Kiwi', 'Jambo', 'Jenipapo', 'Jaca', 'Jabuticaba', 'Groselha', 'Goiaba', 'Framboesa', 'Figo', 'Coco', 'Cereja', 'Carambola', 'Caja', 'Cacau', 'Amora', 'Acerola', 'Acai', 'Abacaxi', 'Abacate', 'Caju']
 const paises = ['Países', 'Brasil', 'Argentina', 'China', 'Uruguai', 'Equador', 'Chile', 'Russia', 'Estados Unidos', 'Russia', 'Australia', 'Japao', 'India', 'Canada', 'Mexico', 'Alemanha', 'Franca', 'Italia', 'Mongolia', 'Suica', 'Paraguai', 'Nigeria', 'Argelia', 'Dinamarca', 'Finlandia', 'Cazaquistao', 'Indonesia', 'Sudao', 'Libia', 'Peru', 'Mali', 'Colombia', 'Etiopia', 'Bolivia', 'Egito', 'Venezuela', 'Iraque', 'Siria', 'Paquistao', 'Afeganistao', 'Somalia', 'Espanha', 'Suecia', 'Tailandia', 'Camaroes', 'Portugal', 'Inglaterra', 'Filipinas', 'Romenia', 'Nepal', 'Grecia', 'Cuba', 'Austria', 'Croacia']
 const temas = [animais,comidas,capitaisBrasileiras, linguagensDeProgramacao, jsFuncional, cores, esportes, instrumentosMusicais, profissões]
+
+
 /*FUNÇÕES DA TELA DE INÍCI0*/
+//Função retorna o tema escolhido
 function mostrarListaDeTemas() {
     const input= document.getElementById('temaInput')
     input.setAttribute('list','lista-temas')
     input.focus();
 }
+
+//Função retorna numero de jogadores
 function mostrarListaN() {
     const input2= document.getElementById('numeroInput')
     input.setAttribute('list','numero-jogadores')
     input.focus();
 }
 
+//Mostra o tema que foi escolhido na tela index.html
 function mostrarTema(lista) {
     const listaNova = lista.toString()
     const indiceAleatorio = Math.floor(Math.random() * lista.length);
@@ -44,16 +40,10 @@ function mostrarTema(lista) {
     return pegarDivTema.textContent= temaAleatorio;    
 }
 mostrarTema(temas)
-//Função para escolher tema
 
-const escolherTema = () => {
-    const escolherTema = document.getElementById('escolherTema')
-    const temaEscolhido = escolherTema.value
-    if (temaEscolhido !== '' ) {
-        document.getElementById('tema').textContent = `Tema Selecionado:' ${temaEscolhido}`
-    }else document.getElementById('tema').textContent = `Tema Não Selecionado`
 
-}
+const players = [{nome: 'Player 1', win: 0, vida: 5}, {nome: 'Player 2', win: 0, vida: 5}, {nome: 'Player 3', win: 0, vida: 5}, {nome: 'Player 4', win: 0, vida: 5}] 
+//Temos separado cada jogador, ao seu lado o número de vitórias e vidas function selecionaPlayer(lista){ //quantidade de players na partida     const pegajog = document.getElementById('numjog').value //pegar o número de jogadores escolhidos     const players = lista.slice(0, pegajog) //selecionar somente a quantidade de players necessária     return players }
 
 //Função que seleciona uma palavra aleatória no tema escolhido
 
@@ -81,20 +71,19 @@ const posicaoLetra = (palavra, letra) => {
     
 }
 
-//console.log(posicaoLetra('teste','e'))
-
 
 //Função para fazer o tratamento da palavra escolhida, ignorando acentuação (coloquei somente para acentuações da língua portuguesa)
 const escolharandom = escolher(animais) //aqui devemos colocar entre parênteses o tema escolhido
 const strip = escolharandom.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
 console.log(strip)
-//Função para separar a string por caracter
-const separa = strip.split("")
 
 const chute = (letra, palavra, vidas)
     if (procurar==0) return chute(letra, palavra, vidas-1) //Errou o chute --> falta implementar uma forma de passar a vez para o próximo jogador, com uma nova entrada de letra, da forma recursiva que está atualmente, vai virar um loop indesejado
     else if (procurar==0 & vidas==0) return gameoverplayer //retornar o fim do jogo --> falta implementar deixar esse jogador "off" até o fim da partida
    // else return tiraletrapalavra & return chute(letra, palavra, vidas) //Acertou o chute --> implementar função para mostrar a letra na palavra e jogador pode jogar dnv
+
+
+
 
 
 

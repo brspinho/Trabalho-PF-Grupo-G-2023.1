b8a4a9dfb6612557644f950cb535841b048ac2d2
GuiFonsec <124678448+GuiFonsec@users.noreply.github.com>
2023-10-08 09:28:00 -0300

    index.js comentado

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const log =(str)=> console.log(str)
-
+//Armazena o tema escolhido pelo usuário na tela inicial da página através do botão select do HTML.
 function pegarTemaURL() {
-    const valorTema= document.getElementById('temas').value
-    log(valorTema)
-    return valorTema
+  const valorTema = document.getElementById("temas").value;
+  return valorTema;
 }
 
+//Armazena o número de jogadores escolhido pelo usuário na tela inicial da página através do botão select do HTML.
 function pegarNumeroJogadores() {
-    const valorN = document.getElementById('numero-jogadores').value
-    log(valorN) 
-    return valorN     
+  const valorN = document.getElementById("numero-jogadores").value;
+  return valorN;
 }
 
-const redirecionar = () =>{
-    const tema = pegarTemaURL()
-    const jogadores = pegarNumeroJogadores()
-    window.location.href = `./game.html?tema=${tema}&jogadores=${jogadores}`
-}
+/* Insere o tema e o número de jogadores escolhido pelo usuário no URL da págino do jogo, 
+transmitindo a informação da página index.html para game.html. A função roda quando o botão
+start game é pressionado */
+const redirecionar = () => {
+  const tema = pegarTemaURL();
+  const jogadores = pegarNumeroJogadores();
+  window.location.href = `./game.html?tema=${tema}&jogadores=${jogadores}`;
+};
